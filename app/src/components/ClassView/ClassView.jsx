d395553bf7fd96ebb39ce312dc09d85dcd1077b5
Maximilian Schulze <maximilian.schulze@student.hpi.de>
2020-06-17 14:33:09 +0200

    add classComment to ClassView

--- app/src/components/ClassView/ClassView.jsx
+++ app/src/components/ClassView/ClassView.jsx
@@ -1,17 +1,22 @@
 import { CircularProgress } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
-import { getAllMethodsOf } from '../../utils/apiHandler';
+import { getAllMethodsOf, getClass } from '../../utils/apiHandler';
 import './ClassView.css';
 
 const ClassView = ({ currentClass }) => {
   const [loading, setLoading] = useState(true);
   const [content, setContent] = useState('');
+  const [hasClassComment, setHasClassComment] = useState(false);
+  const [classComment, setClassComment] = useState('');
 
   useEffect(() => {
     const simpleFetch = async () => {
       const methodsResponse = await getAllMethodsOf(currentClass);
+      const classResponse = await getClass(currentClass);
       setContent(methodsResponse.count.total);
+      setHasClassComment(classResponse.hasClassComment)
+      setClassComment(classResponse.classComment)
       setLoading(false);
     };
     simpleFetch();
@@ -20,6 +25,13 @@ const ClassView = ({ currentClass }) => {
   return (
     <div >
       <h1>{currentClass}</h1>
+      <div className={"comment"}>
+        {hasClassComment ? (
+          <p>
+            {classComment}
+          </p>
+        ) : null}
+      </div>
       {loading ? (
         <CircularProgress />
       ) : (
