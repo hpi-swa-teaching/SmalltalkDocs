b53d644e3f520318fcd8913bb72b5a465b132f1a
phpfs <phpfs@users.noreply.github.com>
2020-07-03 16:32:38 +0200

    First working prototype implementation

---
+++ app/scripts/inline-images.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+const buildFolder = 'build';
+const htmlPath = path.join(buildFolder, './index.html');
+let htmlFile = fs.readFileSync(htmlPath, 'utf8');
+
+const regex = /"(\.?[\w/]+\.(\w{2,5}))"/gm;
+const imageTypes = { png: 'data:image/png', ico: 'data:image/x-icon' };
+
+let grp;
+// eslint-disable-next-line no-cond-assign
+while ((grp = regex.exec(htmlFile)) !== null) {
+  // Necessary to prevent infinite loops with zero-width regex matches
+  if (grp.index === regex.lastIndex) {
+    regex.lastIndex += 1;
+  }
+
+  if (grp.length === 3 && imageTypes[grp[2]] != null) {
+    const fileName = grp[1];
+    const extension = grp[2];
+    const filePath = path.join(buildFolder, fileName);
+    // eslint-disable-next-line no-console
+    console.log('Included:', filePath);
+
+    const base64 = fs.readFileSync(filePath, 'base64');
+    const imgSrc = `${imageTypes[extension]};base64,${base64}`;
+
+    htmlFile = htmlFile.replace(fileName, imgSrc);
+  }
+}
+
+fs.writeFileSync(htmlPath, htmlFile);
+// eslint-disable-next-line no-console
+console.log(`--> exported to ${htmlPath}`);
