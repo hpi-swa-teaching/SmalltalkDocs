2317ececa846691327a29578c4f95a93865af9b9
florian.papsdorf <florian.papsdorf@student.hpi.de>
2020-07-04 12:48:14 +0200

    Work on tests for UncommentedMethodsView.test.js

---
+++ app/src/components/UncommentedMethodsView/UncommentedMethodsView.test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { cleanUpContainer, prepareContainer } from '../../test-utils/test-helper';
+import UncommentedMethodsView from './UncommentedMethodsView';
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = prepareContainer(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  container = cleanUpContainer(container);
+});
+
+describe('UncommentedMethodsView', () => {
+  it('should display a list of classes', () => {
+    act(() => {
+      render(
+        <Router>
+          <UncommentedMethodsView />
+        </Router>,
+        container
+      );
+    });
+
+    expect(container.querySelector('#openSidebarBox')).toBeInTheDocument();
+  });
+
+  it('should display not display the metrics title', () => {
+    const isOpen = false;
+    const toggleIsOpen = () => {};
+
+    act(() => {
+      render(
+        <Router>
+          <MetricsSidebar isOpen={isOpen} toggleIsOpen={toggleIsOpen} />
+        </Router>,
+        container
+      );
+    });
+
+    expect(container.querySelector('#closedSidebarBox')).toBeInTheDocument();
+    expect(container.querySelector('.secondarySidebarTitle')).toBeNull();
+  });
+});
