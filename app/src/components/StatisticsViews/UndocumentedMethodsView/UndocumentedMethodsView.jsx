e9bd81853f3682836640a2b150f9a908a309088e
florian.papsdorf <florian.papsdorf@student.hpi.de>
2020-07-05 21:34:44 +0200

    WIP UndocumentedMethodsView.jsx

--- app/src/components/StatisticsViews/UndocumentedMethodsView/UndocumentedMethodsView.jsx
+++ app/src/components/StatisticsViews/UndocumentedMethodsView/UndocumentedMethodsView.jsx
@@ -1,6 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getUndocumentedMethods } from '../../../utils/apiHandler';
+import ToggleSwitch from '../../ToggleSwitch/ToggleSwitch';
+
 import './UndocumentedMethodsView.css';
 
-const UndocumentedMethodsView = () => <div></div>;
+const UndocumentedMethodsView = () => {
+  const [fetchedMethods, setFetchedMethods] = useState([]);
+  // sortBy=false --> sort by className, sortBy=true --> sort by methodName
+  const [sortBy, setSortBy] = useState(false);
+
+  const changeToggleSortBy = () => setSortBy(!sortBy);
+
+  const sortByMethodNames = (methodInfoA, methodInfoB) =>
+    methodInfoA.methodName.localeCompare(methodInfoB.methodName);
+  const sortByClassNames = (methodInfoA, methodInfoB) =>
+    methodInfoA.className.localeCompare(methodInfoB.className);
+
+  const chooseSortByFunction = sortBy ? sortByMethodNames : sortByClassNames;
+
+  useEffect(() => {
+    const simpleFetch = async () => setFetchedMethods(await getUndocumentedMethods());
+    simpleFetch().then();
+  }, []);
+
+  return (
+    <div>
+      <h1>Undocumented Methods</h1>
+      <ToggleSwitch isActive={sortBy} toggleChange={() => changeToggleSortBy()} />
+      <ul>
+        {fetchedMethods.sort(chooseSortByFunction()).map(aMethodInfo => (
+          <li>
+            {aMethodInfo.className}:{aMethodInfo.methodName}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default UndocumentedMethodsView;
