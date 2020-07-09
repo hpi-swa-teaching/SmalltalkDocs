15673e712722ea3f68f75481cae67a7291f732c0
florian.papsdorf <florian.papsdorf@student.hpi.de>
2020-07-09 23:21:42 +0200

    WIP: Merge explorers analog to statistics

--- app/src/routes/App/App.jsx
+++ app/src/routes/App/App.jsx
@@ -15,22 +15,6 @@ const App = () => (
         <Route exact path="/statistics" component={StatisticsView} />
         <Route exact path="/statistics/:currentMetric" component={StatisticsView} />
         <Route exact path="/doku" component={ExplorationView} />
-        {/* <Route
-          exact
-          path="/doku/help/:currentClass"
-          render={props => <ExplorationView {...props} mode="help" />}
-        />
-        <Route
-          exact
-          path="/doku/classes/:currentClass"
-          render={props => <ExplorationView {...props} mode="class" />}
-        />
-        <Route
-          exact
-          path="/doku/classes/:currentClass/methods/:site/:currentMethod"
-          render={props => <ExplorationView {...props} mode="method" />}
-        />
-        */}
         <Route component={NotFound} />
       </Switch>
     </Suspense>
