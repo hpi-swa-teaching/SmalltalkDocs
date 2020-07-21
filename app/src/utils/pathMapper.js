aa94cbb850bf30b905e790855526034199546a88
florian.papsdorf <florian.papsdorf@student.hpi.de>
2020-07-21 22:31:35 +0200

    Improve pathMappers

--- app/src/utils/pathMapper.js
+++ app/src/utils/pathMapper.js
@@ -1,14 +1,4 @@
-import { baseURL } from '../config/constants';
-
-/*
- * a path is a full qualified location
- * a link is a location given relatively to the origin of the React app
- */
-
 export const getLinkToClass = aClassName => `/doku/classes/${aClassName}`;
 
 export const getLinkToMethod = (aMethodName, aClassName, theSite) =>
   `/doku/classes/${aClassName}/methods/${theSite}/${aMethodName}`;
-
-export const getPathToMethod = (aMethodName, aClassName, theSite) =>
-  `${baseURL}${getLinkToMethod(aMethodName, aClassName, theSite)}`;
