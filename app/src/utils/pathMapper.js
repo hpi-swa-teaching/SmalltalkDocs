e848431e49ac862025baa7130fbe4e26fc0af9b1
florian.papsdorf <florian.papsdorf@student.hpi.de>
2020-07-28 09:58:54 +0200

    Some path refactorings

--- app/src/utils/pathMapper.js
+++ app/src/utils/pathMapper.js
@@ -1,5 +1,10 @@
 import { baseURL } from '../config/constants';
 
-// eslint-disable-next-line import/prefer-default-export
 export const getPathToMethod = (aMethodName, aClassName, theSite) =>
   `${baseURL}/doku/classes/${aClassName}/methods/${theSite}/${aMethodName}`;
+
+export const getPathToClass = aClassName => `/doku/classes/${aClassName}`;
+
+export const getPathToDokuRoot = () => '/doku';
+
+export const getPathToHelpClass = aClassName => `/doku/help/${aClassName}`;
