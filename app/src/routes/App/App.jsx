a591db8fe4afaeccc0490decd2a34c22776de2f3
florian.papsdorf <florian.papsdorf@student.hpi.de>
2020-08-03 22:33:56 +0200

    Introduce path mappers in router

--- app/src/routes/App/App.jsx
+++ app/src/routes/App/App.jsx
@@ -1,6 +1,14 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import {
+  getClassDocumentationURLBluePrint,
+  getMethodDocumentationURLBluePrint,
+  getPathToDokuRoot,
+  getPathToRoot,
+  getPathToStatisticsRoot,
+  getSpecificMetricsURLBluePrint
+} from '../../utils/pathMapper';
 
 const NotFound = lazy(() => import('../NotFound/NotFound'));
 const RootLandingPage = lazy(() => import('../RootLandingPage/RootLandingPage'));
@@ -11,16 +19,12 @@ const App = () => (
   <Router>
     <Suspense fallback={<div>Loading...</div>}>
       <Switch>
-        <Route exact path="/" component={RootLandingPage} />
-        <Route exact path="/statistics" component={StatisticsView} />
-        <Route exact path="/statistics/:currentMetric" component={StatisticsView} />
-        <Route exact path="/doku" component={ExplorationView} />
-        <Route exact path="/doku/classes/:currentClass" component={ExplorationView} />
-        <Route
-          exact
-          path="/doku/classes/:currentClass/methods/:site/:currentMethod"
-          component={ExplorationView}
-        />
+        <Route exact path={getPathToRoot()} component={RootLandingPage} />
+        <Route exact path={getPathToStatisticsRoot()} component={StatisticsView} />
+        <Route exact path={getSpecificMetricsURLBluePrint()} component={StatisticsView} />
+        <Route exact path={getPathToDokuRoot()} component={ExplorationView} />
+        <Route exact path={getClassDocumentationURLBluePrint()} component={ExplorationView} />
+        <Route exact path={getMethodDocumentationURLBluePrint()} component={ExplorationView} />
         <Route component={NotFound} />
       </Switch>
     </Suspense>
