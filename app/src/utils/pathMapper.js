162c712b952dd740506b4a95d4f1b8337f441fba
florian.papsdorf <florian.papsdorf@student.hpi.de>
2020-08-03 22:30:31 +0200

    Implement path mappers

--- app/src/utils/pathMapper.js
+++ app/src/utils/pathMapper.js
@@ -1,8 +1,26 @@
 export const getPathToMethod = (aMethodName, aClassName, theSite) =>
   `/doku/classes/${aClassName}/methods/${theSite}/${aMethodName}`;
-
 export const getPathToClass = aClassName => `/doku/classes/${aClassName}`;
-
 export const getPathToDokuRoot = () => '/doku';
-
 export const getPathToHelpClass = aClassName => `/doku/help/${aClassName}`;
+export const getPathToRoot = () => '/';
+export const getPathToStatisticsRoot = () => '/statistics';
+export const getPathToMetrics = aMetrics => `/statistics/:${aMetrics}`;
+
+const getURLParameterFromKey = key => `:${key}`;
+
+const metricsKey = () => 'currentMetric';
+const classKey = () => 'currentClass';
+const methodKey = () => 'currentMethod';
+const methodSideKey = () => 'methodSide';
+
+export const getSpecificMetricsURLBluePrint = () =>
+  getPathToMetrics(getURLParameterFromKey(metricsKey()));
+export const getClassDocumentationURLBluePrint = () =>
+  getPathToClass(getURLParameterFromKey(classKey()));
+export const getMethodDocumentationURLBluePrint = () =>
+  getPathToMethod(
+    getURLParameterFromKey(methodKey()),
+    getURLParameterFromKey(classKey()),
+    getURLParameterFromKey(methodSideKey())
+  );
