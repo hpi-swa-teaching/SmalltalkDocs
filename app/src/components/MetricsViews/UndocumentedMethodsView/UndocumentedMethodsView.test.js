a44cb3f7f3a64af69caead5c045f8b647fe008d2
Dahye Kim <Dahye.Kim@student.hpi.uni-potsdam.de>
2020-08-07 22:08:50 +0200

    Refactor UndocumentedMethodsView test

--- app/src/components/MetricsViews/UndocumentedMethodsView/UndocumentedMethodsView.test.js
+++ app/src/components/MetricsViews/UndocumentedMethodsView/UndocumentedMethodsView.test.js
@@ -2,30 +2,46 @@ import React from 'react';
 import { render } from 'react-dom';
 import { act } from 'react-dom/test-utils';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { getUndocumentedMethodsOfClassAPIMock } from '../../../test-utils/apiMocks';
+import { baseURL } from '../../../config/constants';
+import { cleanUpContainer, prepareContainer } from '../../../test-utils/test-helper';
+import { getFetchClassesMock } from '../../../test-utils/apiMocks';
 import UndocumentedMethodsView from './UndocumentedMethodsView';
 
-let container;
 
-beforeEach(() => {
-  container = document.createElement('div');
-  document.body.appendChild(container);
-});
+describe('UncommentedMethodsView', () => {
+  const fetchMock = getFetchClassesMock();
 
-afterEach(() => {
-  document.body.removeChild(container);
-  container = null;
-});
+  let container = null;
+  beforeEach(async () => {
+    // setup a DOM element as a render target
+    container = prepareContainer(container);
+    await act(async () => {
+      render(
+        <Router>
+          <UndocumentedMethodsView />
+        </Router>,
+        container
+      );
+    });
+  });
 
-xtest('Should display the correct title', async () => {
-  getUndocumentedMethodsOfClassAPIMock();
-  await act(async () =>
-    render(
-      <Router>
-        <UndocumentedMethodsView />
-      </Router>,
-      container
-    )
-  );
-  expect(container).toHaveTextContent('Undocumented Methods');
+  afterEach(() => {
+    jest.clearAllMocks();
+    container = cleanUpContainer(container);
+  });
+  
+  it('should display fetched classes', async () => {
+    expect(container).toHaveTextContent('RPTestClass');
+    expect(container).toHaveTextContent('X509TBSCertificate');
+    expect(container).toHaveTextContent('MetacelloAllowProjectUpgrade');
+  });
+
+  it('should display headline', async () => {
+    expect(container).toHaveTextContent('Undocumented Methods');
+  });  
+
+  it('should request API only once', () => expect(fetchMock).toBeCalledTimes(1));
+
+  it('should request API with correct path', () =>
+    expect(fetchMock).toHaveBeenCalledWith(`${baseURL}/env/classes`));
 });
